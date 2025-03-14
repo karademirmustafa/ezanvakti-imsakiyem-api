482508d092391506b08c40b9e3af9b5bf682d324
karademirmustafa <karademirbayburt@gmail.com>
2025-03-14 12:16:24 +0000

    Update: Modified 'app.listen' in server.ts to bind to '127.0.0.1' for Nginx proxy compatibility

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import { app } from './app';
 import "./types/request";
 import "./types/express";
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const PORT = parseInt(process.env.PORT) || 3000;
+
+app.listen(PORT, '127.0.0.1', () => {
+    console.log(`Server running on http://127.0.0.1:${PORT}`);
 });
